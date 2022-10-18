4781133ae0efdbfb53f28ecdea9641961e2ef6ed
XuToWei <xw741619720@qq.com>
2022-10-18 21:11:34 +0800

    fix gen

--- Develop/Excel/gen_json.sh
+++ Develop/Excel/gen_json.sh
@@ -11,7 +11,7 @@ UNITY_OUTPUT_DATA_DIR=${WORKSPACE}/Unity/Assets/Res/Config
 GEN_TYPE_CODE_DATA=code_cs_unity_json,data_json
 GEN_TYPE_DATA=data_bin
 
-${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
+dotnet ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_code_dir ${OUTPUT_CODE_DIR}/Client/Config \
@@ -19,14 +19,14 @@ ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
  --gen_types ${GEN_TYPE_CODE_DATA} \
  -s client
 
-${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
+dotnet ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_data_dir ${UNITY_OUTPUT_DATA_DIR}/Client \
  --gen_types ${GEN_TYPE_DATA} \
  -s client
 
-${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/Load -j cfg --\
+dotnet ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/Load -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_code_dir ${OUTPUT_CODE_DIR}/Server/Config \
@@ -34,14 +34,14 @@ ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/Load -j cfg --\
  --gen_types ${GEN_TYPE_CODE_DATA} \
  -s server
 
-${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/Load -j cfg --\
+dotnet ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/Load -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_data_dir ${UNITY_OUTPUT_DATA_DIR}/Server \
  --gen_types ${GEN_TYPE_DATA} \
  -s server
 
-${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
+dotnet ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_code_dir ${OUTPUT_CODE_DIR}/ClientServer/Config \
@@ -49,21 +49,21 @@ ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
  --gen_types ${GEN_TYPE_CODE_DATA} \
  -s clientserver
 
-${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
+dotnet ${GEN_CLIENT} --template_search_path ${CUSTOM_TEMPLATE_DIR}/LoadAsync -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_data_dir ${UNITY_OUTPUT_DATA_DIR}/ClientServer \
  --gen_types ${GEN_TYPE_DATA} \
  -s clientserver
 
-${GEN_CLIENT} -j cfg --\
+dotnet ${GEN_CLIENT} -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_code_dir ${WORKSPACE}/Unity/Assets/Scripts/Editor/Luban \
  --gen_types  code_cs_unity_editor_json \
  -s editor
 
-${GEN_CLIENT} -j cfg --\
+dotnet ${GEN_CLIENT} -j cfg --\
  -d ${CONF_ROOT}/Defines/__root__.xml \
  --input_data_dir ${CONF_ROOT}/Datas \
  --output_data_dir ${WORKSPACE}/Unity/Assets/Res/Editor/Luban \
