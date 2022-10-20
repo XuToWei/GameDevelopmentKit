d03bb1ddd0b2e7485d30b69cece3f7a469dffe5e
XuToWei <xw741619720@qq.com>
2022-10-21 02:05:23 +0800

    添加多语言功能，自动导入

---
+++ Unity/Assets/Scripts/Editor/ThirdParty/Localization/LocalizationTool.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UGF;
+using UnityEditor;
+using UnityEngine;
+using OfficeOpenXml;
+
+namespace I2.Loc
+{
+    /// <summary>
+    /// 自动从Localization.xlsx中更新本地化数据
+    /// </summary>
+    public static class LocalizationTool
+    {
+        private static string LocalizationFileDirectory { get; } = $"{Application.dataPath}/../../Develop/";
+        private static string LocalizationFileName { get; } = "Localization.xlsx";
+        private static string LocalizationFilePath { get; set; }
+        private static FileSystemWatcher m_Watcher;
+        private const string LoadTimeSaveKey = "Editor.LocalizationLoadTime";
+
+        [InitializeOnLoadMethod]
+        public static void OnInitialize()
+        {
+            LocalizationFilePath = Path.Combine(LocalizationFileDirectory, LocalizationFileName);
+            
+            m_Watcher = new FileSystemWatcher();
+            m_Watcher.Filter = LocalizationFileName;
+            m_Watcher.Path = LocalizationFileDirectory;
+            m_Watcher.IncludeSubdirectories = false;
+            m_Watcher.EnableRaisingEvents = true;
+
+            m_Watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+            m_Watcher.Changed += OnCSVFileChanged;
+
+            if (PlayerPrefs.HasKey(LoadTimeSaveKey))//放置代码修改时反复加载
+            {
+                FileInfo excelFileInfo = new FileInfo(LocalizationFilePath);
+                if (PlayerPrefs.GetString(LoadTimeSaveKey) != excelFileInfo.LastWriteTime.ToString())
+                {
+                    LoadFromCSV();
+                }
+            }
+            else
+            {
+                Loom.Post(LoadFromCSV);
+            }
+        }
+
+        [MenuItem("Tools/更新本地化")]
+        public static void LoadFromCSV()
+        {
+            LanguageSource languageSource = AssetDatabase.LoadAssetAtPath<LanguageSource>(AssetUtility.GetLocalizationAsset());
+            languageSource.mSource.Import_CSV(string.Empty, ExcelToCSV(LocalizationFilePath), eSpreadsheetUpdateMode.Replace);
+            EditorUtility.SetDirty(languageSource);
+            AssetDatabase.Refresh();
+            FileInfo excelFileInfo = new FileInfo(LocalizationFilePath);
+            PlayerPrefs.SetString(LoadTimeSaveKey, excelFileInfo.LastWriteTime.ToString());
+            Debug.Log("本地化xlsx已加载！");
+        }
+
+        private static void OnCSVFileChanged(object sender, FileSystemEventArgs e)
+        {
+            Loom.Post(LoadFromCSV);
+        }
+
+        private static List<string[]> ExcelToCSV(string excelFilePath)
+        {
+            List<string[]> csv = new List<string[]>();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using FileStream fileStream = new FileStream(excelFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using ExcelPackage excelPackage = new ExcelPackage(fileStream);
+            for (int i = 0; i < excelPackage.Workbook.Worksheets.Count; i++)
+            {
+                ExcelWorksheet sheet = excelPackage.Workbook.Worksheets[i];
+                if (sheet.Name.Contains("~"))
+                    continue;
+                if(sheet.Dimension.End.Row < 2)
+                    continue;
+                int rawColumnCount = 0;
+                bool hasEmpty = false;
+                for (int j = 1; j <= sheet.Dimension.End.Column; j++)
+                {
+                    if (!hasEmpty)
+                    {
+                        if (sheet.Cells[1, j].Value == null)
+                        {
+                            hasEmpty = true;
+                            continue;
+                        }
+                        rawColumnCount++;
+                    }
+                    else
+                    {
+                        if (sheet.Cells[1, j].Value != null)
+                        {
+                            throw new Exception($"{excelFilePath}：第1行有为空的列！");
+                        }
+                    }
+                }
+                
+                for (int row = 1; row <= sheet.Dimension.End.Row; row++)
+                {
+                    if(row == 2)//第二行为注释，跳过
+                        continue;
+                    string[] strArray = new string[rawColumnCount];
+                    for (int clo = 1; clo <= rawColumnCount; clo++)
+                    {
+                        object value = sheet.Cells[row, clo].Value;
+                        if (value == null)
+                        {
+                            strArray[clo - 1] = string.Empty;
+                        }
+                        else
+                        {
+                            strArray[clo - 1] = value.ToString();
+                        }
+                    }
+                    csv.Add(strArray);
+                }
+            }
+
+            return csv;
+        }
+    }
+}
--- Unity/Assets/Scripts/Loader/UGF/Procedure/ProcedurePreload.cs
+++ Unity/Assets/Scripts/Loader/UGF/Procedure/ProcedurePreload.cs
@@ -1,17 +1,28 @@
+using ET;
 using GameFramework.Fsm;
 using GameFramework.Procedure;
+using I2.Loc;
+using UnityEngine;
 
 namespace UGF
 {
     public class ProcedurePreload : ProcedureBase
     {
-        protected override void OnEnter(IFsm<IProcedureManager> procedureOwner)
+        protected override async void OnEnter(IFsm<IProcedureManager> procedureOwner)
         {
             base.OnEnter(procedureOwner);
             
             UnityGameFramework.Runtime.Log.Debug("Enter preload procedure!");
+
+            await this.LoadLocalization();
             
             ChangeState<ProcedurePreloadET>(procedureOwner);
+        }
+
+        private async ETTask LoadLocalization()
+        {
+            GameObject languageSourceObj = await GameEntry.Resource.LoadAssetAsync<GameObject>(AssetUtility.GetLocalizationAsset());
+            GameObject.Instantiate(languageSourceObj).name = "LocalizationSource";
         }
     }
 }
