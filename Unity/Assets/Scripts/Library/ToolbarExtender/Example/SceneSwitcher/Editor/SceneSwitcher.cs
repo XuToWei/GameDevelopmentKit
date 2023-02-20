a00df72980cb3054593562ee22963f5a1d9e79a9
XuToWei <xw741619720@qq.com>
2023-02-20 19:27:22 +0800

    add toolbar

---
+++ Unity/Assets/Scripts/Library/ToolbarExtender/Example/SceneSwitcher/Editor/SceneSwitcher.cs
@@ -0,0 +1,93 @@
+﻿using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace UnityToolbarExtender.Examples
+{
+	static class ToolbarStyles
+	{
+		public static readonly GUIStyle commandButtonStyle;
+
+		static ToolbarStyles()
+		{
+			commandButtonStyle = new GUIStyle("Command")
+			{
+				fontSize = 16,
+				alignment = TextAnchor.MiddleCenter,
+				imagePosition = ImagePosition.ImageAbove,
+				fontStyle = FontStyle.Bold
+			};
+		}
+	}
+
+	[InitializeOnLoad]
+	public class SceneSwitchLeftButton
+	{
+		static SceneSwitchLeftButton()
+		{
+			ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+		}
+
+		static void OnToolbarGUI()
+		{
+			// GUILayout.FlexibleSpace();
+			//
+			// if(GUILayout.Button(new GUIContent("1", "Start Scene 1"), ToolbarStyles.commandButtonStyle))
+			// {
+			// 	SceneHelper.StartScene("ToolbarExtenderExampleScene1");
+			// }
+			//
+			// if(GUILayout.Button(new GUIContent("2", "Start Scene 2"), ToolbarStyles.commandButtonStyle))
+			// {
+			// 	SceneHelper.StartScene("ToolbarExtenderExampleScene2");
+			// }
+		}
+	}
+
+	static class SceneHelper
+	{
+		static string sceneToOpen;
+
+		public static void StartScene(string sceneName)
+		{
+			if(EditorApplication.isPlaying)
+			{
+				EditorApplication.isPlaying = false;
+			}
+
+			sceneToOpen = sceneName;
+			EditorApplication.update += OnUpdate;
+		}
+
+		static void OnUpdate()
+		{
+			if (sceneToOpen == null ||
+			    EditorApplication.isPlaying || EditorApplication.isPaused ||
+			    EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode)
+			{
+				return;
+			}
+
+			EditorApplication.update -= OnUpdate;
+
+			if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+			{
+				// need to get scene via search because the path to the scene
+				// file contains the package version so it'll change over time
+				string[] guids = AssetDatabase.FindAssets("t:scene " + sceneToOpen, null);
+				if (guids.Length == 0)
+				{
+					Debug.LogWarning("Couldn't find scene file");
+				}
+				else
+				{
+					string scenePath = AssetDatabase.GUIDToAssetPath(guids[0]);
+					EditorSceneManager.OpenScene(scenePath);
+					EditorApplication.isPlaying = true;
+				}
+			}
+			sceneToOpen = null;
+		}
+	}
+}
