2b6ac8f2a1e01a40c5e5d40e12edb88e178133c0
XuToWei <xw741619720@qq.com>
2023-02-21 13:31:13 +0800

    优化DotNet目录

--- Share/Analyzer/Config/AnalyzeAssembly.cs
+++ Share/Analyzer/Config/AnalyzeAssembly.cs
@@ -2,7 +2,7 @@
 {
     public static class AnalyzeAssembly
     {
-        private const string DotNet_ET = "DotNet.ET";
+        private const string DotNet_Library = "DotNet.Library";
         private const string DotNet_Model = "DotNet.Model";
         private const string DotNet_Hotfix = "DotNet.Hotfix";
 
@@ -31,7 +31,7 @@
         
         public static readonly string[] All =
         {
-            DotNet_ET, DotNet_Model, DotNet_Hotfix,
+            DotNet_Library, DotNet_Model, DotNet_Hotfix,
             Unity_ET_Runtime, Unity_ET_Loader,
             Unity_ET_Code_Model, Unity_ET_Code_ModelView, Unity_ET_Code_Hotfix, Unity_ET_Code_HotfixView
         };
