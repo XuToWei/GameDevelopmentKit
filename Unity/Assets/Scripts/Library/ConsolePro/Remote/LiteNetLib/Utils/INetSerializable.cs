0c1d60233e770e2d3315a07b2503944bd44eb599
XuToWei <xw741619720@qq.com>
2023-03-14 17:04:55 +0800

    delete ConsolePro，有需要自行添加

--- Unity/Assets/Scripts/Library/ConsolePro/Remote/LiteNetLib/Utils/INetSerializable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FlyingWormConsole3.LiteNetLib.Utils
-{
-    public interface INetSerializable
-    {
-        void Serialize(NetDataWriter writer);
-        void Deserialize(NetDataReader reader);
-    }
-}
