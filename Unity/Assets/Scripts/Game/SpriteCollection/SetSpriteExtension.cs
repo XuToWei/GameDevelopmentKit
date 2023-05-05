e1c05529d5e92067464387e1f7ee273440f6175a
XuToWei <xw741619720@qq.com>
2023-05-05 18:14:14 +0800

    优化

--- Unity/Assets/Scripts/Game/SpriteCollection/SetSpriteExtension.cs
+++ Unity/Assets/Scripts/Game/SpriteCollection/SetSpriteExtension.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine.UI;
 using UnityGameFramework.Extension;
 
@@ -11,9 +12,9 @@ namespace Game
         /// <param name="image"></param>
         /// <param name="collectionPath">精灵所在收集器地址</param>
         /// <param name="spritePath">精灵名称</param>
-        public static void SetSpriteAsync(this Image image, string collectionPath, string spritePath)
+        public static UniTask SetSpriteAsync(this Image image, string collectionPath, string spritePath)
         {
-            GameEntry.SpriteCollection.SetSpriteAsync(WaitSetImage.Create(image, collectionPath, spritePath));
+            return GameEntry.SpriteCollection.SetSpriteAsync(WaitSetImage.Create(image, collectionPath, spritePath));
         }
         
         /// <summary>
