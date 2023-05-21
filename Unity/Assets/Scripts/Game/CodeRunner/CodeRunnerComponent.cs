618aa874f38a34bfd15a8ff7d5bab73624b8137f
XuToWei <xw741619720@qq.com>
2023-05-21 22:22:49 +0800

    优化CodeRunner

--- Unity/Assets/Scripts/Game/CodeRunner/CodeRunnerComponent.cs
+++ Unity/Assets/Scripts/Game/CodeRunner/CodeRunnerComponent.cs
@@ -11,17 +11,22 @@ namespace Game
         [ShowInInspector, ReadOnly]
         public bool IsRunning { get; private set; } = false;
 
+        [ShowInInspector, ReadOnly]
         private Component m_InitComponent;
         
         public void StartRun(string startMonoType)
         {
+            if (IsRunning)
+            {
+                throw new GameFrameworkException("CodeRunnerComponent StartRun duplicate!");
+            }
             Type initType = Utility.Assembly.GetType(startMonoType);
             if (initType == null)
             {
                 throw new GameFrameworkException($"Not Found {startMonoType}!");
             }
-            this.m_InitComponent = gameObject.AddComponent(initType);
-            if (this.m_InitComponent == null)
+            m_InitComponent = gameObject.AddComponent(initType);
+            if (m_InitComponent == null)
             {
                 throw new GameFrameworkException($"Add {initType} Fail!");
             }
@@ -30,6 +35,10 @@ namespace Game
 
         public void Shutdown()
         {
+            if (!IsRunning)
+            {
+                throw new GameFrameworkException("CodeRunnerComponent can Shutdown only when is running!");
+            }
             IsRunning = false;
             DestroyImmediate(this.m_InitComponent);
         }
