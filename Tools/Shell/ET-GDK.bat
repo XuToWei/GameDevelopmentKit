0dac5439baf6cdec312b32744326fea9e3fde842
XuToWei <xw741619720@qq.com>
2023-10-20 00:02:52 +0800

    添加更新ET工具

---
+++ Tools/Shell/ET-GDK.bat
@@ -0,0 +1,14 @@
+set SOURCE=..\..\..\ET
+set DESTINATION=..\..\..\GameDevelopmentKit
+
+rd/s /q %DESTINATION%\Unity\Assets\Scripts\Game\ET\Code\ModelView
+rd/s /q %DESTINATION%\Unity\Assets\Scripts\Game\ET\Code\Model
+rd/s /q %DESTINATION%\Unity\Assets\Scripts\Game\ET\Code\HotfixView
+rd/s /q %DESTINATION%\Unity\Assets\Scripts\Game\ET\Code\Hotfix
+
+xcopy %SOURCE%\Unity\Assets\Scripts\ModelView %DESTINATION%\Unity\Assets\Scripts\Game\ET\Code\ModelView /s /e
+xcopy %SOURCE%\Unity\Assets\Scripts\Model %DESTINATION%\Unity\Assets\Scripts\Game\ET\Code\Model /s /e
+xcopy %SOURCE%\Unity\Assets\Scripts\HotfixView %DESTINATION%\Unity\Assets\Scripts\Game\ET\Code\HotfixView /s /e
+xcopy %SOURCE%\Unity\Assets\Scripts\Hotfix %DESTINATION%\Unity\Assets\Scripts\Game\ET\Code\Hotfix /s /e
+
+pause
---
+++ Tools/Shell/GDK-ET.bat
@@ -0,0 +1,14 @@
+set SOURCE=..\..\..\GameDevelopmentKit
+set DESTINATION=..\..\..\ET
+
+rd/s /q %DESTINATION%\Unity\Assets\Scripts\ModelView
+rd/s /q %DESTINATION%\Unity\Assets\Scripts\Model
+rd/s /q %DESTINATION%\Unity\Assets\Scripts\HotfixView
+rd/s /q %DESTINATION%\Unity\Assets\Scripts\Hotfix
+
+xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\ModelView %DESTINATION%\Unity\Assets\Scripts\ModelView /s /e
+xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\Model %DESTINATION%\Unity\Assets\Scripts\Model /s /e
+xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\HotfixView %DESTINATION%\Unity\Assets\Scripts\HotfixView /s /e
+xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\Hotfix %DESTINATION%\Unity\Assets\Scripts\Hotfix /s /e
+
+pause
