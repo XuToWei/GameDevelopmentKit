b50684022a2770552aba9348a641aa3cfb5018ab
XuToWei <xw741619720@qq.com>
2023-10-21 11:22:19 +0800

    ET更新脚本去掉文件夹确认

--- Tools/Shell/GDK-ET.bat
+++ Tools/Shell/GDK-ET.bat
@@ -6,9 +6,9 @@ rd/s /q %DESTINATION%\Unity\Assets\Scripts\Model
 rd/s /q %DESTINATION%\Unity\Assets\Scripts\HotfixView
 rd/s /q %DESTINATION%\Unity\Assets\Scripts\Hotfix
 
-xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\ModelView %DESTINATION%\Unity\Assets\Scripts\ModelView /s /e
-xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\Model %DESTINATION%\Unity\Assets\Scripts\Model /s /e
-xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\HotfixView %DESTINATION%\Unity\Assets\Scripts\HotfixView /s /e
-xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\Hotfix %DESTINATION%\Unity\Assets\Scripts\Hotfix /s /e
+xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\ModelView %DESTINATION%\Unity\Assets\Scripts\ModelView /s /e /i
+xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\Model %DESTINATION%\Unity\Assets\Scripts\Model /s /e /i
+xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\HotfixView %DESTINATION%\Unity\Assets\Scripts\HotfixView /s /e /i
+xcopy %SOURCE%\Unity\Assets\Scripts\Game\ET\Code\Hotfix %DESTINATION%\Unity\Assets\Scripts\Hotfix /s /e /i
 
 pause
