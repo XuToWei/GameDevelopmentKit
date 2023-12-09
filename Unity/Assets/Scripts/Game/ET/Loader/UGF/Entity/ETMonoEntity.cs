cc0b4c8c37163f0f150917a7f9ef34f3cd5ccb13
XuToWei <xw741619720@qq.com>
2023-12-09 11:53:11 +0800

    优化ETMonoEntity和ETMonoUIForm

--- Unity/Assets/Scripts/Game/ET/Loader/UGF/Entity/ETMonoEntity.cs
+++ Unity/Assets/Scripts/Game/ET/Loader/UGF/Entity/ETMonoEntity.cs
@@ -10,13 +10,23 @@ namespace ET
     {
         private UGFEntity m_UGFEntity;
         private string m_EntityEventTypeName;
-        private IUGFEntityEvent ugfEntityEvent => UGFEventComponent.Instance.GetEntityEvent(this.m_EntityEventTypeName);
+        private IUGFEntityEvent m_EntityEvent;
+
         public bool isShow { get; private set; }
         public UGFEntity ugfEntity => this.m_UGFEntity;
 
         public void OnReload()
         {
-            ugfEntityEvent.OnReload(this.m_UGFEntity);
+            if(UGFEventComponent.Instance.TryGetEntityEvent(this.m_EntityEventTypeName, out IUGFEntityEvent entityEvent))
+            {
+                this.m_EntityEvent = entityEvent;
+            }
+            else
+            {
+                this.m_EntityEvent = default;
+                throw new GameFrameworkException($"EntityEventType {this.m_EntityEventTypeName} doesn't exist EntityEvent!");
+            }
+            this.m_EntityEvent.OnReload(this.m_UGFEntity);
         }
 
         protected override void OnShow(object userData)
@@ -30,12 +40,21 @@ namespace ET
             if (this.m_UGFEntity == default || this.m_EntityEventTypeName != entityData.EntityEventTypeName || entityData.ParentEntity != this.m_UGFEntity.Parent)
             {
                 UGFEntityDispose();
+                if(UGFEventComponent.Instance.TryGetEntityEvent(entityData.EntityEventTypeName, out IUGFEntityEvent entityEvent))
+                {
+                    this.m_EntityEvent = entityEvent;
+                }
+                else
+                {
+                    this.m_EntityEvent = default;
+                    throw new GameFrameworkException($"EntityEventType {this.m_EntityEventTypeName} doesn't exist EntityEvent!");
+                }
                 this.m_EntityEventTypeName = entityData.EntityEventTypeName;
                 this.m_UGFEntity = entityData.ParentEntity.AddChild<UGFEntity, string, ETMonoEntity>(this.m_EntityEventTypeName, this);
-                this.ugfEntityEvent.OnInit(this.m_UGFEntity, entityData.UserData);
+                this.m_EntityEvent.OnInit(this.m_UGFEntity, entityData.UserData);
             }
             this.isShow = true;
-            this.ugfEntityEvent.OnShow(this.m_UGFEntity, entityData.UserData);
+            this.m_EntityEvent.OnShow(this.m_UGFEntity, entityData.UserData);
             entityData.Release();
         }
 
@@ -56,7 +75,7 @@ namespace ET
 
         protected override void OnHide(bool isShutdown, object userData)
         {
-            this.ugfEntityEvent.OnHide(this.m_UGFEntity, isShutdown, userData);
+            this.m_EntityEvent.OnHide(this.m_UGFEntity, isShutdown, userData);
             this.isShow = false;
             if (isShutdown)
             {
@@ -68,37 +87,37 @@ namespace ET
         protected override void OnAttached(EntityLogic childEntity, Transform parentTransform, object userData)
         {
             base.OnAttached(childEntity, parentTransform, userData);
-            this.ugfEntityEvent.OnAttached(this.m_UGFEntity, childEntity, parentTransform, userData);
+            this.m_EntityEvent.OnAttached(this.m_UGFEntity, childEntity, parentTransform, userData);
         }
 
         protected override void OnDetached(EntityLogic childEntity, object userData)
         {
             base.OnDetached(childEntity, userData);
-            this.ugfEntityEvent.OnDetached(this.m_UGFEntity, childEntity, userData);
+            this.m_EntityEvent.OnDetached(this.m_UGFEntity, childEntity, userData);
         }
 
         protected override void OnAttachTo(EntityLogic parentEntity, Transform parentTransform, object userData)
         {
             base.OnAttachTo(parentEntity, parentTransform, userData);
-            this.ugfEntityEvent.OnAttachTo(this.m_UGFEntity, parentEntity, parentTransform, userData);
+            this.m_EntityEvent.OnAttachTo(this.m_UGFEntity, parentEntity, parentTransform, userData);
         }
 
         protected override void OnDetachFrom(EntityLogic parentEntity, object userData)
         {
             base.OnDetachFrom(parentEntity, userData);
-            this.ugfEntityEvent.OnDetachFrom(this.m_UGFEntity, parentEntity, userData);
+            this.m_EntityEvent.OnDetachFrom(this.m_UGFEntity, parentEntity, userData);
         }
 
         protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
         {
             base.OnUpdate(elapseSeconds, realElapseSeconds);
-            this.ugfEntityEvent.OnUpdate(this.m_UGFEntity, elapseSeconds, realElapseSeconds);
+            this.m_EntityEvent.OnUpdate(this.m_UGFEntity, elapseSeconds, realElapseSeconds);
         }
 
         protected override void OnRecycle()
         {
             base.OnRecycle();
-            this.ugfEntityEvent.OnRecycle(this.m_UGFEntity);
+            this.m_EntityEvent.OnRecycle(this.m_UGFEntity);
         }
     }
 }
--- Unity/Assets/Scripts/Game/ET/Loader/UGF/UIForm/ETMonoUIForm.cs
+++ Unity/Assets/Scripts/Game/ET/Loader/UGF/UIForm/ETMonoUIForm.cs
@@ -9,13 +9,23 @@ namespace ET
     {
         private UGFUIForm m_UGFUIForm;
         private int m_UIFormId;
-        private IUGFUIFormEvent ugfUIFormEvent => UGFEventComponent.Instance.GetUIFormEvent(this.m_UIFormId);
+        private IUGFUIFormEvent m_UIFormEvent;
+
         public bool isOpen { get; private set; }
         public UGFUIForm ugfUIForm => this.m_UGFUIForm;
 
         public void OnReload()
         {
-            ugfUIFormEvent.OnReload(this.m_UGFUIForm);
+            if(UGFEventComponent.Instance.TryGetUIFormEvent(this.m_UIFormId, out IUGFUIFormEvent uiFormEvent))
+            {
+                this.m_UIFormEvent = uiFormEvent;
+            }
+            else
+            {
+                this.m_UIFormEvent = default;
+                throw new GameFrameworkException($"UIFormId {this.m_UIFormId} doesn't exist UIFormEvent!");
+            }
+            this.m_UIFormEvent.OnReload(this.m_UGFUIForm);
         }
 
         protected override void OnOpen(object userData)
@@ -29,12 +39,21 @@ namespace ET
             if (this.m_UGFUIForm == default || this.m_UIFormId != formData.UIFormId || formData.ParentEntity != this.m_UGFUIForm.Parent)
             {
                 UGFUIFormDispose();
+                if(UGFEventComponent.Instance.TryGetUIFormEvent(formData.UIFormId, out IUGFUIFormEvent uiFormEvent))
+                {
+                    this.m_UIFormEvent = uiFormEvent;
+                }
+                else
+                {
+                    this.m_UIFormEvent = default;
+                    throw new GameFrameworkException($"UIFormId {this.m_UIFormId} doesn't exist UIFormEvent!");
+                }
                 this.m_UIFormId = formData.UIFormId;
                 this.m_UGFUIForm = formData.ParentEntity.AddChild<UGFUIForm, int, ETMonoUIForm>(this.m_UIFormId, this);
-                this.ugfUIFormEvent.OnInit(this.m_UGFUIForm, formData.UserData);
+                this.m_UIFormEvent.OnInit(this.m_UGFUIForm, formData.UserData);
             }
             this.isOpen = true;
-            this.ugfUIFormEvent.OnOpen(this.m_UGFUIForm, formData.UserData);
+            this.m_UIFormEvent.OnOpen(this.m_UGFUIForm, formData.UserData);
             formData.Release();
         }
 
@@ -56,7 +75,7 @@ namespace ET
         protected override void OnClose(bool isShutdown, object userData)
         {
             this.isOpen = false;
-            this.ugfUIFormEvent.OnClose(this.m_UGFUIForm, isShutdown, userData);
+            this.m_UIFormEvent.OnClose(this.m_UGFUIForm, isShutdown, userData);
             if (isShutdown)
             {
                 UGFUIFormDispose();
@@ -67,49 +86,49 @@ namespace ET
         protected override void OnPause()
         {
             base.OnPause();
-            this.ugfUIFormEvent.OnPause(this.m_UGFUIForm);
+            this.m_UIFormEvent.OnPause(this.m_UGFUIForm);
         }
 
         protected override void OnResume()
         {
             base.OnResume();
-            this.ugfUIFormEvent.OnResume(this.m_UGFUIForm);
+            this.m_UIFormEvent.OnResume(this.m_UGFUIForm);
         }
 
         protected override void OnCover()
         {
             base.OnCover();
-            this.ugfUIFormEvent.OnCover(this.m_UGFUIForm);
+            this.m_UIFormEvent.OnCover(this.m_UGFUIForm);
         }
 
         protected override void OnReveal()
         {
             base.OnReveal();
-            this.ugfUIFormEvent.OnReveal(this.m_UGFUIForm);
+            this.m_UIFormEvent.OnReveal(this.m_UGFUIForm);
         }
 
         protected override void OnRefocus(object userData)
         {
             base.OnRefocus(userData);
-            this.ugfUIFormEvent.OnRefocus(this.m_UGFUIForm, userData);
+            this.m_UIFormEvent.OnRefocus(this.m_UGFUIForm, userData);
         }
 
         protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
         {
             base.OnUpdate(elapseSeconds, realElapseSeconds);
-            this.ugfUIFormEvent.OnUpdate(this.m_UGFUIForm, elapseSeconds, realElapseSeconds);
+            this.m_UIFormEvent.OnUpdate(this.m_UGFUIForm, elapseSeconds, realElapseSeconds);
         }
 
         protected override void OnDepthChanged(int uiGroupDepth, int depthInUIGroup)
         {
             base.OnDepthChanged(uiGroupDepth, depthInUIGroup);
-            this.ugfUIFormEvent.OnDepthChanged(this.m_UGFUIForm, uiGroupDepth, depthInUIGroup);
+            this.m_UIFormEvent.OnDepthChanged(this.m_UGFUIForm, uiGroupDepth, depthInUIGroup);
         }
 
         protected override void OnRecycle()
         {
             base.OnRecycle();
-            this.ugfUIFormEvent.OnRecycle(this.m_UGFUIForm);
+            this.m_UIFormEvent.OnRecycle(this.m_UGFUIForm);
         }
     }
 }
