f4933f9e79003ad6e6ae81879297e813ffb03f41
XuToWei <xw741619720@qq.com>
2024-01-18 12:42:37 +0800

    修复删除插件脚本

--- Tools/Shell/delete no free unity plugins.bat
+++ Tools/Shell/delete no free unity plugins.bat
@@ -1,14 +1,14 @@
 set DESTINATION=..\..\Unity\Assets
 
-rd \s \q %DESTINATION%\Scripts\Library\EnhancedScroller
-rd \s \q %DESTINATION%\Scripts\Library\SLATE Cinematic Sequencer
-rd \s \q %DESTINATION%\Scripts\Library\SmartUiSelection
-rd \s \q %DESTINATION%\Scripts\Library\StompyRobot
-rd \s \q %DESTINATION%\Scripts\Plugins\Demigiant
-rd \s \q %DESTINATION%\Scripts\Plugins\Sirenix
+rd/s /q %DESTINATION%\Scripts\Library\EnhancedScroller
+rd/s /q "%DESTINATION%\Scripts\Library\SLATE Cinematic Sequencer"
+rd/s /q %DESTINATION%\Scripts\Library\SmartUiSelection
+rd/s /q %DESTINATION%\Scripts\Library\StompyRobot
+rd/s /q %DESTINATION%\Scripts\Plugins\Demigiant
+rd/s /q %DESTINATION%\Scripts\Plugins\Sirenix
 
 del %DESTINATION%\Scripts\Library\EnhancedScroller.meta
-del %DESTINATION%\Scripts\Library\SLATE Cinematic Sequencer.meta
+del "%DESTINATION%\Scripts\Library\SLATE Cinematic Sequencer.meta"
 del %DESTINATION%\Scripts\Library\SmartUiSelection.meta
 del %DESTINATION%\Scripts\Library\StompyRobot.meta
 del %DESTINATION%\Scripts\Plugins\Demigiant.meta
