70b6e58985f38367f9e7b71c56f4489cf709c303
XuToWei <xw741619720@qq.com>
2024-12-03 01:19:39 +0800

    修复Unity6设置声音播放位置警告

--- Unity/Assets/Scripts/Library/UGF/UnityGameFramework/Runtime/Sound/DefaultSoundAgentHelper.cs
+++ Unity/Assets/Scripts/Library/UGF/UnityGameFramework/Runtime/Sound/DefaultSoundAgentHelper.cs
@@ -59,7 +59,14 @@ namespace UnityGameFramework.Runtime
             }
             set
             {
+#if UNITY_6000_0_OR_NEWER
+                if (m_AudioSource.clip != null)
+                {
+                    m_AudioSource.time = value;
+                }
+#else
                 m_AudioSource.time = value;
+#endif
             }
         }
 
