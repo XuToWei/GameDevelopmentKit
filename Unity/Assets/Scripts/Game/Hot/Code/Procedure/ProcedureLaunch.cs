379f0646c7d88d8189033aa6e91212cb0d151b76
XuToWei <xw741619720@qq.com>
2025-12-11 21:06:31 +0800

    优化protobuf成0gc

--- Unity/Assets/Scripts/Game/Hot/Code/Procedure/ProcedureLaunch.cs
+++ Unity/Assets/Scripts/Game/Hot/Code/Procedure/ProcedureLaunch.cs
@@ -1,4 +1,7 @@
+using System;
+using GameFramework;
 using GameFramework.Fsm;
+using ProtoBuf;
 
 namespace Game.Hot
 {
@@ -7,6 +10,15 @@ namespace Game.Hot
         protected override void OnEnter(IFsm<ProcedureComponent> procedureOwner)
         {
             base.OnEnter(procedureOwner);
+            // 注册ProtoBuf自定义生成器
+            ProtoActivator.RegisterCustomFactory((type, nonPublic) =>
+            {
+                if (type.IsClass)
+                {
+                    return ReferencePool.Acquire(type);
+                }
+                return Activator.CreateInstance(type, nonPublic);
+            });
         }
 
         protected override void OnUpdate(IFsm<ProcedureComponent> procedureOwner, float elapseSeconds, float realElapseSeconds)
